fa847bb706cec720842bb5eb54b3db1347025773
Chaitanya Dagar <dagarchaitanya1@gmail.com>
2018-10-16 11:24:29 +0530

    Updates

---
+++ precache-manifest.9f759ff06964d295cca260e06343fc7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c8173cb2ef23652c9b3",
+    "url": "/onlineshopp/static/js/runtime~main.6c8173cb.js"
+  },
+  {
+    "revision": "8fb9689128540651dd2c",
+    "url": "/onlineshopp/static/js/main.8fb96891.chunk.js"
+  },
+  {
+    "revision": "831790b8c9ef615b0ba6",
+    "url": "/onlineshopp/static/js/1.831790b8.chunk.js"
+  },
+  {
+    "revision": "8fb9689128540651dd2c",
+    "url": "/onlineshopp/static/css/main.af991829.chunk.css"
+  },
+  {
+    "revision": "831790b8c9ef615b0ba6",
+    "url": "/onlineshopp/static/css/1.1b4caa2f.chunk.css"
+  },
+  {
+    "revision": "341e8029f4f5d63a6d0c073d142ff329",
+    "url": "/onlineshopp/index.html"
+  }
+];
